6a9b764d6fb639bf75ca98fe951a6dc84070870e
adhis <tiva.adhisti@ui.ac.id>
2023-11-05 23:33:09 +0700

    completed assignment 7

---
+++ lib/menu.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  final List<InvItem> items = [
+    InvItem("View Items", Icons.checklist,
+        Colors.blue), // Set the color for each item
+    InvItem("Add Items", Icons.add_shopping_cart, Colors.green),
+    InvItem("Logout", Icons.logout, Colors.red),
+  ];
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'shopVentory',
+        ),
+      ),
+      body: SingleChildScrollView(
+        // Scrolling wrapper widget
+        child: Padding(
+          padding: const EdgeInsets.all(10.0), // Set padding for the page
+          child: Column(
+            // Widget to display children vertically
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                // Text widget to display text with center alignment and appropriate style
+                child: Text(
+                  'Inventory Management', // Text indicating the shop name
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              // Grid layout
+              GridView.count(
+                // Container for our cards.
+                primary: true,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                children: items.map((InvItem item) {
+                  // Iteration for each item
+                  return ShopCard(item);
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ShopCard extends StatelessWidget {
+  final InvItem item;
+
+  const ShopCard(this.item, {Key? key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: item.buttonColor,
+      child: InkWell(
+        // Responsive touch area
+        onTap: () {
+          // Show a SnackBar when clicked
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+                content: Text("You clicked the ${item.name} button!")));
+        },
+        child: Container(
+          // Container to hold Icon and Text
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class InvItem {
+  final String name;
+  final IconData icon;
+  final Color buttonColor;
+
+  InvItem(this.name, this.icon, this.buttonColor);
+
+  // Rest of the class remains the same
+}
